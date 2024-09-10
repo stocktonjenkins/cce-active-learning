ec99126798dc3c364b949e7980e15b2ac35e14cd
Spencer Young <spencer.young@deliciousai.com>
2024-09-10 09:04:43 -0600

    Added ViT backbone for image tasks

--- probcal/models/backbones.py
+++ probcal/models/backbones.py
@@ -1,5 +1,7 @@
 import torch
 from torch import nn
+from transformers import ViTModel
+from transformers.modeling_outputs import BaseModelOutputWithPooling
 
 
 class Backbone(nn.Module):
@@ -44,3 +46,60 @@ class MLP(Backbone):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.layers(x)
+
+
+class MNISTCNN(Backbone):
+    """A CNN feature extractor for the MNIST dataset (1x28x28 image tensors).
+
+    Attributes:
+        output_dim (int, optional): Dimension of output feature vectors. Defaults to 64.
+    """
+
+    def __init__(self, output_dim: int = 64):
+        super(MNISTCNN, self).__init__(output_dim=output_dim)
+
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.flat = nn.Flatten()
+        self.fc1 = nn.Linear(5 * 5 * 64, output_dim * 2)
+        self.fc2 = nn.Linear(output_dim * 2, output_dim)
+        self.relu = nn.ReLU()
+        self.max_pool = nn.MaxPool2d(2)
+        self.dropout = nn.Dropout(p=0.5)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.bn1(self.max_pool(self.relu(self.conv1(x))))
+        x = self.bn2(self.max_pool(self.relu(self.conv2(x))))
+        x = self.dropout(self.flat(x))
+        x = self.dropout(self.relu(self.fc1(x)))
+        x = self.relu(self.fc2(x))
+        return x
+
+
+class ViT(Backbone):
+    """A ViT feature extractor for images.
+
+    Attributes:
+        output_dim (int): Dimension of output feature vectors.
+    """
+
+    def __init__(self, output_dim: int = 64):
+        """Initialize a ViT image feature extractor.
+
+        Args:
+            output_dim (int, optional): Dimension of output feature vectors. Defaults to 64.
+        """
+        super(ViT, self).__init__(output_dim=output_dim)
+        self.backbone = ViTModel.from_pretrained("google/vit-base-patch16-224-in21k")
+        self.projection_1 = nn.Linear(768, 384)
+        self.projection_2 = nn.Linear(384, self.output_dim)
+        self.relu = nn.ReLU()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        outputs: BaseModelOutputWithPooling = self.backbone(pixel_values=x)
+        h = outputs.pooler_output
+        h = self.relu(self.projection_1(h))
+        h = self.relu(self.projection_2(h))
+        return h
