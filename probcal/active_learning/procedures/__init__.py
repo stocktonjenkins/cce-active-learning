7b9731e8063d93fbc67435e10121672548dd1af4
RISHABH SHARMA <rsharma228@instgpu-04.cs.wisc.edu>
2024-12-01 02:09:28 -0600

    add confidence active learning procedure

--- probcal/active_learning/procedures/__init__.py
+++ probcal/active_learning/procedures/__init__.py
@@ -1,6 +1,9 @@
 from probcal.active_learning.configs import ActiveLearningConfig
 from probcal.active_learning.procedures.cce_active_learning_procedure import (
-    CCEProcedure,
+    CCEProcedure
+)
+from probcal.active_learning.procedures.confidence_active_learning_procedure import (
+    ConfidenceProcedure
 )
 from probcal.active_learning.procedures.random_procedure import RandomProcedure
 
@@ -10,6 +13,8 @@ def get_active_learning_procedure(config: ActiveLearningConfig):
         return RandomProcedure
     if config.procedure_type == "cce":
         return CCEProcedure
+    if config.procedure_type == "confidence":
+        return ConfidenceProcedure
     else:
         raise ValueError(
             f"Unknown active learning procedure type: {config.procedure_type}"
---
+++ probcal/active_learning/procedures/confidence_active_learning_procedure.py
@@ -0,0 +1,74 @@
+import lightning
+import numpy as np
+import torch
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+
+from probcal.active_learning.active_learning_types import (
+    ActiveLearningEvaluationResults,
+    ModelAccuracyResults,
+)
+from probcal.active_learning.procedures.base import (
+    ActiveLearningProcedure,
+)
+from probcal.evaluation.calibration_evaluator import CalibrationResults
+from probcal.models.discrete_regression_nn import DiscreteRegressionNN
+
+
+class ConfidenceProcedure(ActiveLearningProcedure[ActiveLearningProcedure]):
+    def get_next_label_set(
+        self, unlabeled_indices: np.ndarray, k: int, model: DiscreteRegressionNN
+    ) -> np.ndarray:
+        """
+        Randomly choose the next set of indices to add to label set
+        Args:
+            unlabeled_indices: np.ndarray
+            k: int
+            model: DiscreteRegressionNN
+
+        Returns:
+            A random subset of unlabeled indices.
+        """
+        unlabeled_dataloader = self.dataset.unlabeled_dataloader()
+        confindence_unlabelled = self.get_confidence_score_from_model(
+            model,
+            data_loader=unlabeled_dataloader,
+        )
+        assert confindence_unlabelled.shape[0] == len(unlabeled_indices)
+        _, sampling_indices = torch.topk(confindence_unlabelled, k)
+
+        return np.random.choice(sampling_indices, size=k, replace=False)
+
+    def _eval_impl(
+        self, trainer: lightning.Trainer, model: DiscreteRegressionNN
+    ) -> ActiveLearningEvaluationResults:
+        calibration_results: CalibrationResults = self.cal_evaluator(
+            model, data_module=self.dataset
+        )
+        results = trainer.test(model, datamodule=self.dataset)
+        model_accuracy_results = ModelAccuracyResults(**results[0])
+        return ActiveLearningEvaluationResults(
+            calibration_results=calibration_results,
+            model_accuracy_results=model_accuracy_results,
+            iteration=self._iteration,
+            train_set_size=self.dataset.train_indices.shape[0],
+            val_set_size=self.dataset.val_indices.shape[0],
+            unlabeled_set_size=self.dataset.unlabeled_indices.shape[0],
+        )
+    
+    def get_confidence_score_from_model(
+        self, model: DiscreteRegressionNN, data_loader: DataLoader
+    ) -> tuple[torch.Tensor]:
+        with torch.no_grad():
+            conf = []
+            for inputs, _ in tqdm(
+                    data_loader, desc="doing forward pass to compute confidence..."
+            ):
+                
+                y_hat = model.predict(inputs.to(self.device))
+                (mu, var) = torch.split(y_hat, [1, 1], dim=-1)
+                conf.append(var.flatten())
+
+            conf = torch.cat(conf).float()
+
+            return conf
