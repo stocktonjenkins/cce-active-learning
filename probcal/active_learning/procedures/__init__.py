fe8141a9ad851c804d12f0e32be6ca8679d3b5d8
Gurudatta Patil <99181690+gurudatta-patil@users.noreply.github.com>
2024-12-06 09:31:34 -0600

    Update __init__.py

--- probcal/active_learning/procedures/__init__.py
+++ probcal/active_learning/procedures/__init__.py
@@ -6,7 +6,9 @@ from probcal.active_learning.procedures.random_procedure import RandomProcedure
 from probcal.active_learning.procedures.bait_procedure import (
     BAITProcedure,
 )
-
+from probcal.active_learning.procedures.badge_procedure import (
+    BadgeProcedure,
+)
 def get_active_learning_procedure(config: ActiveLearningConfig):
     if config.procedure_type == "random":
         return RandomProcedure
@@ -14,6 +16,8 @@ def get_active_learning_procedure(config: ActiveLearningConfig):
         return CCEProcedure
     if config.procedure_type == "bait":
         return BAITProcedure
+    if config.procedure_type == "badge":
+        return BadgeProcedure
     else:
         raise ValueError(
             f"Unknown active learning procedure type: {config.procedure_type}"
