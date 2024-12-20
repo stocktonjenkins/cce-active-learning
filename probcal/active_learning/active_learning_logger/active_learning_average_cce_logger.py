eafe648ae96ae825a3199472b5f7cc41075a04e7
stocktonjenkins <stockton.jenkins@deliciousai.com>
2024-12-20 12:05:13 -0700

    chore: fix cce logger

--- probcal/active_learning/active_learning_logger/active_learning_average_cce_logger.py
+++ probcal/active_learning/active_learning_logger/active_learning_average_cce_logger.py
@@ -2,6 +2,7 @@ import csv
 from pathlib import Path
 
 import numpy as np
+import pandas as pd
 from filelock import FileLock
 
 from probcal.active_learning.active_learning_types import (
@@ -35,22 +36,21 @@ class ActiveLearningAverageCCELogger(IObserver):
         state = subject.get_state()
         assert state.calibration_results is not None, "Calibration results not saved!"
         with FileLock(self.lock_path):
-            with open(self.path, mode="a", newline="") as file:
-                writer = csv.writer(file)
-                writer.writerow(
-                    [
-                        state.kth_trial,
-                        state.iteration,
-                        state.train_set_size,
-                        state.val_set_size,
-                        state.test_set_size,
-                        state.unlabeled_set_size,
-                        np.concatenate(
-                            [
-                                vals.mcmd_vals
-                                for vals in state.calibration_results.mcmd_results
-                            ]
-                        ).mean(),
-                        state.calibration_results.ece,
-                    ]
-                )
+            df = pd.read_csv(self.path)
+            df.loc[len(df)] = [
+                state.kth_trial,
+                state.iteration,
+                state.train_set_size,
+                state.val_set_size,
+                state.test_set_size,
+                state.unlabeled_set_size,
+                np.concatenate(
+                    [vals.mcmd_vals for vals in state.calibration_results.mcmd_results]
+                ).mean(),
+                state.calibration_results.ece,
+            ]
+            df = df.round(
+                {col: 4 for col in df.select_dtypes(include=["float"]).columns}
+            )
+            df.drop_duplicates(inplace=True)
+            df.to_csv(self.path, index=False)
