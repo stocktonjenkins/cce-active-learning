398fc4ca6b86b391cf6a1081af791f99f3530bbd
stocktonjenkins <stockton.jenkins@deliciousai.com>
2024-12-31 07:48:47 -0700

    feat: embeddings for  LCMD method

--- probcal/active_learning/procedures/lcmd/lcmd_procedure.py
+++ probcal/active_learning/procedures/lcmd/lcmd_procedure.py
@@ -4,7 +4,10 @@ from probcal.active_learning.active_learning_types import (
     ActiveLearningEvaluationResults,
 )
 from probcal.active_learning.procedures.base import ActiveLearningProcedure
-from probcal.active_learning.procedures.lcmd.feature_data import TensorFeatureData
+from probcal.active_learning.procedures.lcmd.feature_data import (
+    TensorFeatureData,
+    SubsetFeatureData,
+)
 from probcal.active_learning.procedures.lcmd.feature_maps import IdentityFeatureMap
 from probcal.active_learning.procedures.lcmd.features import Features
 from probcal.active_learning.procedures.lcmd.selection import (
@@ -17,16 +20,8 @@ class LCMDProcedure(ActiveLearningProcedure[ActiveLearningEvaluationResults]):
     def get_next_label_set(
         self, unlabeled_indices: np.ndarray, k: int, model: DiscreteRegressionNN
     ) -> np.ndarray:
-        # TODO: use self.dataset.full_dataset to get embeddings from CLIP?
-        #       how would this work for text?
-        self.dataset.full_dataset
-        X = TensorFeatureData(
-            data=[...]  # TODO: tensor of (n_samples, n_features)
-        )
-        feature_data = {
-            "train": X[self.dataset.train_indices],
-            "pool": X[unlabeled_indices],
-        }
+        breakpoint()
+        feature_data = self.get_tensor_features(model)
         feature_map = IdentityFeatureMap(
             n_features=feature_data["train"].get_tensor(0).shape[-1]
         )
@@ -39,3 +34,13 @@ class LCMDProcedure(ActiveLearningProcedure[ActiveLearningEvaluationResults]):
         )
         selected_indices = alg.select(batch_size=1).detach().cpu().numpy()
         return unlabeled_indices[selected_indices]
+
+    def get_tensor_features(
+        self, model: DiscreteRegressionNN
+    ) -> dict[str, TensorFeatureData]:
+        train = self.get_embedding(model, loader=self.dataset.train_dataloader())
+        pool = self.get_embedding(model, loader=self.dataset.unlabeled_dataloader())
+        return {
+            "pool": TensorFeatureData(pool),
+            "train": TensorFeatureData(train),
+        }
