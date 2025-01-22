ceacb2651fa5d7a8182bd98aa5c14eff30740066
Stockton Jenkins <stocktonjenkins@gmail.com>
2025-01-22 15:04:29 -0800

    add text to al config

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-PYTHONPATH="../cce-active-learning" CUDA_VISIBLE_DEVICES=1 python probcal/active_learning/main.py --train-config=configs/train/reviews/gaussian_cfg.yaml --procedure='random'
+PYTHONPATH="../cce-active-learning" CUDA_VISIBLE_DEVICES=1 python probcal/active_learning/main.py --train-config=configs/train/reviews/gaussian_cfg.yaml --procedure='cce'
